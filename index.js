73d0229d11446f9d4c687448859d497c39504469
Kalvin Tang <ktang82@my.bcit.ca>
2024-07-15 02:02:15 -0700

    moved loop out of game logic, added handle picks function to handle the choice logis

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-// Play function
-function play() {
-    // playRound();
-    //
-}
+// set rounds
+let round = 0;
+const maxRound = 5;
 
 // CPU choice logic
-
 function cpuPick() {
     // gets number between 1 and 3
     let x = Math.floor(Math.random() * 3) + 1;
@@ -13,81 +10,85 @@ function cpuPick() {
     return x;
 }
 
+// CPU image update
+function updateCpuImage(choice) {
+    const cpuCardImage = document.querySelector(".cpu__card img");
+    let fileName;
+
+    if (choice === 1) {
+        fileName = "Scissor";
+    } else if (choice === 2) {
+        fileName = "Rock";
+    } else {
+        fileName = "Paper";
+    }
+
+    cpuCardImage.src = `./assets/images/${fileName}.jpg`;
+}
+
 // Player choice logic
-function numberPick() {
-    let input;
+function humanPick() {
+    const scissorButton = document.getElementById("scissor-btn");
+    const rockButton = document.getElementById("rock-btn");
+    const paperButton = document.getElementById("paper-btn");
 
-    while (true) {
-        input = prompt(
-            "Enter a choice:\n\n 1 = Scissors\n\n 2 = Rock\n\n 3 = Paper\n\n 4 = Exit"
-        );
-
-        // convert string to number
-
-        input = Number(input);
-
-        // condition to handle incorrect input to loop until correct input
-
-        if (input === 1 || input === 2 || input === 3 || input == 4) {
-            break;
+    // handle choice logic
+    function handlePicks(playerChoice, imageName) {
+        if (round < maxRound) {
+            const playerCard = document.querySelector(".player__card img");
+            playerCard.src = `./assets/images/${imageName}.jpg`;
+            playRound(playerChoice);
         } else {
-            alert("Invalid choice, Please enter a valid number between 1 to 3");
+            document.getElementById("results").innerHTML = "Game Over!";
+            console.log("game over");
         }
     }
 
-    console.log(`Player number: ${input}`);
-    return input;
+    scissorButton.addEventListener("click", () => handlePicks(1, "Scissor"));
+    rockButton.addEventListener("click", () => handlePicks(2, "Rock"));
+    paperButton.addEventListener("click", () => handlePicks(3, "Paper"));
 }
 
-function playRound(cpuChoice, playerChoice) {
-    // Loop counter for round
-    let round = 1;
+function playRound(playerChoice) {
+    // set round counter
+    round++;
+    // Displays round
+    document.getElementById("round").textContent = round;
 
-    // Logic for winning
+    // CPU choice
+    cpuChoice = cpuPick();
+    updateCpuImage(cpuChoice);
 
-    for (let i = 1; i <= 5; i++) {
-        // Gets player choice  function
-        playerChoice = numberPick();
-        cpuChoice = cpuPick();
-
-        // If player exits
-        if (playerChoice == 4) {
-            console.log("GoodBye!");
-            break;
-        }
-
-        // Displays round
-        console.log(`Round: ${round}`);
-
-        // all outcomes for human wins
-        // if cpu choice is 1 (scissors) and human is 1 (rock)
-        // if cpu choice is 2 (rock) and human is 3 (paper)
-        // if cput choice is 3 (paper) and human is 1 (scissors)
-        if (
-            (cpuChoice === 1 && playerChoice === 2) ||
-            (cpuChoice === 2 && playerChoice === 3) ||
-            (cpuChoice == 3 && playerChoice === 1)
-        ) {
-            console.log(`CPU choice: ${cpuChoice}`);
-            console.log(`Human choice: ${playerChoice}`);
-            console.log("You win!");
-        }
-        // if CPU choice is equal to human input. Its a tie
-        else if (cpuChoice === playerChoice) {
-            console.log(`CPU choice: ${cpuChoice}`);
-            console.log(`Human choice: ${playerChoice}`);
-            console.log("It is a draw!");
-        }
-        // Else CPU wins.
-        else {
-            console.log(`CPU choice: ${cpuChoice}`);
-            console.log(`Human choice: ${playerChoice}`);
-            console.log("You lose!");
-        }
-        round++;
+    // all outcomes for human wins
+    // if cpu choice is 1 (scissors) and human is 1 (rock)
+    // if cpu choice is 2 (rock) and human is 3 (paper)
+    // if cput choice is 3 (paper) and human is 1 (scissors)
+    if (
+        (cpuChoice === 1 && playerChoice === 2) ||
+        (cpuChoice === 2 && playerChoice === 3) ||
+        (cpuChoice == 3 && playerChoice === 1)
+    ) {
+        document.getElementById("results").innerHTML = "You Win! ";
+        console.log(`CPU choice: ${cpuChoice}`);
+        console.log(`Human choice: ${playerChoice}`);
+        console.log("You win!");
+    }
+    // if CPU choice is equal to human input. Its a tie
+    else if (cpuChoice === playerChoice) {
+        document.getElementById("results").innerHTML = "It's a tie!";
+        console.log(`CPU choice: ${cpuChoice}`);
+        console.log(`Human choice: ${playerChoice}`);
+        console.log("It is a draw!");
+    }
+    // Else CPU wins.
+    else {
+        document.getElementById("results").innerHTML = "You lose!";
+        console.log(`CPU choice: ${cpuChoice}`);
+        console.log(`Human choice: ${playerChoice}`);
+        console.log("You lose!");
     }
 }
 
-play();
-
-// Win logic
+document.addEventListener("DOMContentLoaded", function () {
+    humanPick();
+});
